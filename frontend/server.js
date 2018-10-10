2c9fb16b1f27ec3808c817989dac6d3911fc49d1
Timo Buerk <timo.buerk@bfh.ch>
2018-10-10 12:22:57 +0200

    second try heroku deploy frontend

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,16 @@
-// server.js
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-// Run the app by serving the static files
-// in the dist directory
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/ESE-Angular-Frontend'));
 
-app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req,res) {
 
-// Start the app by listening on the default
-// Heroku port
+    res.sendFile(path.join(__dirname+'/dist/ESE-Angular-Frontend/index.html'));
+});
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
