877cde5b24664f14663ca1cf2417a8cb04d634c8
Simone Aellen <simoneaellen@stud-183-163.eduroam.unibe.ch>
2018-10-31 16:40:49 +0100

    implemented user login

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import {JoblistingComponent} from './joblisting/joblisting.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {CreatejoblistComponent} from './createjoblist/createjoblist.component';
 import {JoblistdetailComponent} from './joblistdetail/joblistdetail.component';
+import {LoginComponent} from './login/login.component';
+import {AuthGuard} from './login/login.authguard';
 
 
 const routes: Routes = [
-  {path: '', component: DashboardComponent},
+  {path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   {path: 'createjoblist', component: CreatejoblistComponent},
-  {path: 'joblistdetail', component: JoblistdetailComponent}
-  ];
+  {path: 'joblistdetail', component: JoblistdetailComponent},
+  {path: 'login', component: LoginComponent },
+
+];
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],
