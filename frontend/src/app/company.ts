a9dcae9486b35c9bf0af9b4206da5555905a9444
Suada <suada.abukar@students.unibe.ch>
2018-11-05 12:25:10 +0100

    Update company, jopPensum, joblisting and user.

---
+++ frontend/src/app/company.ts
@@ -0,0 +1,14 @@
+export class Company {
+
+  constructor(
+    public id,
+    public companyName,
+    public companyStreet,
+    public companyZIP,
+    public companyCity,
+    public companyPhone,
+    public companyPerson
+  ) {
+
+  }
+}
--- frontend/src/app/joblisting.ts
+++ frontend/src/app/joblisting.ts
@@ -3,8 +3,19 @@ export class JobListing {
     constructor(
         public id: number,
         public title: string,
-        public description: string
+        public description: string,
+        public isVerified: boolean,
+        public brancheId: number,
+        public jobPensumFrom: number,
+        public jobPensumTo: number,
+        public payment: number,
+        public companyId: number,
+        public contactPerson: string,
+        public contactPhone: string,
+        public contactEmail: string
+
     ) {
     }
 
 }
+
