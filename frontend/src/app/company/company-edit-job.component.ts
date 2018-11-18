d99de4ee8a2d0adbec4530d66874430fae6136e4
Suada <suada.abukar@students.unibe.ch>
2018-11-18 22:27:32 +0100

    Added components for company

---
+++ frontend/src/app/company/company-edit-job.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-edit-job',
+  templateUrl: './company-edit-job.component.html',
+  styleUrls: ['./company-edit-job.component.css']
+})
+export class CompanyEditJobComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/company/company-joblist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-company-joblist',
+  templateUrl: './company-joblist.component.html',
+  styleUrls: ['./company-joblist.component.css']
+})
+export class CompanyJoblistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
