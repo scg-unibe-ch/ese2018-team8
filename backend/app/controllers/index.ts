08480d7e0e77b64123bbe993fb7f23c3923c85de
Timo Buerk <timo.buerk@bfh.ch>
2018-12-09 21:04:26 +0100

    removed deleted models from index.ts

--- backend/app/controllers/index.ts
+++ backend/app/controllers/index.ts
@@ -1,7 +1,4 @@
 export * from './joblisting.controller';
-export * from './skill.controller';
 export * from './company.controller';
-export * from './jobPensum.controller';
-export * from './sector.controller';
 export * from './user.controller';
 export * from './auth.controller';
